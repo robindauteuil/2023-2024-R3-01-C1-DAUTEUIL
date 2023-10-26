e412ac73779b50d7a886340d605f8cc3afe4a29e
robindauteuil <robin.dauteuil@live.fr>
2023-10-26 10:50:22 +0200

    class model

--- index.php
+++ index.php
@@ -9,6 +9,6 @@ $controler = new MainController();
 $controler->index();
 
 
-
+ 
 
 ?>
--- models/Model.php
+++ models/Model.php
@@ -1,14 +1,35 @@
 <?php
 
+
+
 abstract class Model{
 
-    private PDO db;
+    private PDO $db;
 
-    abstract function execRequest(string $sql, array $params = null) : PDOStatement|false
+    function execRequest(string $sql, array $params = null) : PDOStatement|false{
+
+        if ($params){
+            $req = $this->db->prepare($sql);
+            $result = $req->execute($sql, $params);
+        }
+        else{
+            $result = $this->db->query($sql);
+
+        }
+        
+        return $result; 
+
+    }
     
     
     
-    private getDB() : PDO{
+    private function getDB() : PDO{
+
+
+
+        $this->db = new PDO('mysql:host=localhost;dbname=grp-433_S3_progweb', 'grp-433', 'i6nQhpSp', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+        return $this->db ;
         
     }
 }
